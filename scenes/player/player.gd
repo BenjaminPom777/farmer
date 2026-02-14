39234626aeecab097f5921365c2ba7fa6fd578ec
beni <mamont4444@live.ru>
2026-02-14 14:19:38 +0200

    before animation finished tree as it is not available mby in 4.6

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,25 +1,50 @@
 extends CharacterBody2D
 
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get('parameters/MoveStateMachine/playback')
+@onready var tool_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get('parameters/ToolStateMachine/playback')
 
 var direction: Vector2
 var speed := 300
 
+enum Tools {HOE, AXE , WATER}
+var current_tool: Tools = Tools.AXE
+const tool_connection = {
+	Tools.HOE: 'hoe',
+	Tools.AXE: 'axe',
+	Tools.WATER: 'water',
+}
+
+var can_move := true
+
+
 func _physics_process(_delta: float) -> void:
-	get_input()
-	velocity = direction * speed
+	if can_move:
+		get_input()
+	velocity = direction * speed * int(can_move)
 	move_and_slide()
 	animation()
 	
 func get_input():
 	direction = Input.get_vector("left",'right',"up","down")
+	
+	
+	if Input.is_action_just_pressed("tool_forward") or Input.is_action_just_pressed("tool_backward"):
+		var tool_direction = Input.get_axis("tool_backward","tool_forward") as int
+		current_tool = posmod(current_tool + tool_direction, Tools.size()) as Tools
+	
+	if Input.is_action_just_pressed("action"):		
+		tool_state_machine.travel(tool_connection[current_tool])
+		$AnimationTree.set('parameters/OneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 
+	
 func animation():
 	if direction:
 		move_state_machine.travel('move')
 		var target_vector: Vector2 = Vector2(round(direction.x), round (direction.y))
 		$AnimationTree.set('parameters/MoveStateMachine/idle/blend_position',target_vector)
 		$AnimationTree.set("parameters/MoveStateMachine/move/blend_position",target_vector)
+		for state in tool_connection.values():
+			$AnimationTree.set("parameters/ToolStateMachine/"+ state +"/blend_position", target_vector)					
 	else:
 		move_state_machine.travel('idle')
